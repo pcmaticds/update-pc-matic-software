7e65cc56d1c912d5def74cda6775db28f7e59828
pcmaticds <kjhgdhu@outlook.com>
2026-07-11 11:53:54 +0530

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,20 @@
+project = 'How-to-Update-Pc-matic-Software'
+author = 'How-to-Update-Pc-matic-Software'
+release = '1.0'
+
+# Extensions
+extensions = [
+    'sphinx_sitemap',
+]
+
+# Paths
+templates_path = ['_templates']
+exclude_patterns = []
+
+# Theme
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
+# Custom JS & Favicon
+html_js_files = ['chatbot.js']  # chatbot widget
+html_favicon = '_static/favicon.png'
